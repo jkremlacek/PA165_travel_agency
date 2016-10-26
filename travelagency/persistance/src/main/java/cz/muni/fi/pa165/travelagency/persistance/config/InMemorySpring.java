56e0b0f09ab331948b7815259fbab237544a0cd7
Šalata Martin <Martin.Salata@ysoft.com>
2016-10-26 10:14:28 +0200

    spring and hibernate reconfigured

--- travelagency/persistance/src/main/java/cz/muni/fi/pa165/travelagency/persistance/config/InMemorySpring.java
+++ travelagency/persistance/src/main/java/cz/muni/fi/pa165/travelagency/persistance/config/InMemorySpring.java
@@ -9,6 +9,7 @@ import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver
 import org.springframework.instrument.classloading.LoadTimeWeaver;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -25,11 +26,23 @@ import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.
  * @author Martin Salata
  */
 @Configuration
+@ComponentScan(basePackages = "cz.muni.fi.pa165.travelagency.persistance.dao")
+@EnableTransactionManagement
+@EnableJpaRepositories
 public class InMemorySpring {
 
     @Bean
-    public LocalEntityManagerFactoryBean entityManagerFactory(){
-        return new LocalEntityManagerFactoryBean();
+    public JpaTransactionManager transactionManager() {
+        return new JpaTransactionManager(entityManagerFactory().getObject());
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean =
+                new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(this.db());
+        entityManagerFactoryBean.setPersistenceProvider(new HibernatePersistenceProvider());
+        return entityManagerFactoryBean;
     }
 
 
