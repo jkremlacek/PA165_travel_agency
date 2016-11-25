7cfc372c0690d5d76f3eb228d5da809266a0bb1d
Šalata Martin <Martin.Salata@ysoft.com>
2016-11-25 14:35:26 +0100

    ValidationException checking added

--- service/src/main/java/cz/muni/fi/pa165/travelagency/service/UserServiceImpl.java
+++ service/src/main/java/cz/muni/fi/pa165/travelagency/service/UserServiceImpl.java
@@ -11,6 +11,8 @@ import java.util.List;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import javax.inject.Inject;
+import javax.validation.ValidationException;
+
 import org.springframework.stereotype.Service;
 
 /**
@@ -82,7 +84,7 @@ public class UserServiceImpl implements UserService {
     public List<User> findByReservation(Reservation reservation) {
         try {
             return userDao.findByReservation(reservation);
-        } catch (NullPointerException ex) {
+        } catch (NullPointerException | ValidationException ex) {
             throw ex;
         } catch (Exception ex) {
             throw new PersistenceException(ex.getMessage());
@@ -104,7 +106,7 @@ public class UserServiceImpl implements UserService {
     public void delete(User user) {
         try {
             userDao.delete(user);
-        } catch (NullPointerException ex) {
+        } catch (NullPointerException | ValidationException ex) {
             throw ex;
         } catch (Exception ex) {
             throw new PersistenceException(ex.getMessage());
@@ -116,7 +118,7 @@ public class UserServiceImpl implements UserService {
         try {
             user.setPasswordHash(createHash(password));
             userDao.create(user);
-        } catch (NullPointerException ex) {
+        } catch (NullPointerException| ValidationException ex) {
             throw ex;
         } catch (Exception ex) {
             throw new PersistenceException(ex.getMessage());
@@ -139,7 +141,7 @@ public class UserServiceImpl implements UserService {
     public void update(User user) {
         try {
             userDao.update(user);
-        } catch (NullPointerException ex) {
+        } catch (NullPointerException | ValidationException ex) {
             throw ex;
         } catch (Exception ex) {
             throw new PersistenceException(ex.getMessage());
@@ -150,7 +152,7 @@ public class UserServiceImpl implements UserService {
     public Boolean isUserAdmin(User user) {
         try {
             return user.getIsAdmin();
-        } catch (NullPointerException ex) {
+        } catch (NullPointerException | ValidationException ex) {
             throw ex;
         } catch (Exception ex) {
             throw new PersistenceException(ex.getMessage());
@@ -161,14 +163,14 @@ public class UserServiceImpl implements UserService {
     public boolean userAuthenticate(User user, String password) {
         try {
             return validatePassword(password, user.getPasswordHash());
-        } catch (NullPointerException ex) {
+        } catch (NullPointerException | ValidationException ex) {
             throw ex;
         } catch (Exception ex) {
             throw new PersistenceException(ex.getMessage());
         }
     }
     
-        private static String createHash(String password) {
+    private static String createHash(String password) {
         final int sizeOfsalt = 24;
         final int sizeOfhash = 24;
         final int iterations = 1000;
@@ -189,7 +191,7 @@ public class UserServiceImpl implements UserService {
     }
 
     public static boolean validatePassword(String password, String correctHash) {
-        if (password == null) return false;
+        if (password == null) throw new NullPointerException("password cannot be null");
         if (correctHash == null) throw new IllegalArgumentException("password hash is null");
         String[] params = correctHash.split(":");
         int iterations = Integer.parseInt(params[0]);
