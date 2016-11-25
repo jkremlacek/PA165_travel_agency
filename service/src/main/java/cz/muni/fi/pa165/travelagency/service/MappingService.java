64c07ac9819ac2a589eb8551e80547c36380bdfe
Šalata Martin <Martin.Salata@ysoft.com>
2016-11-25 11:02:16 +0100

    javadoc for MappingService added

--- service/src/main/java/cz/muni/fi/pa165/travelagency/service/MappingService.java
+++ service/src/main/java/cz/muni/fi/pa165/travelagency/service/MappingService.java
@@ -9,7 +9,21 @@ import java.util.List;
  * @author Martin Salata
  */
 public interface MappingService {
+    /**
+     * Maps a collection of objects to objects of given type
+     * @param objects objects to be mapped
+     * @param mapToClass class to be mapped to
+     * @param <T>
+     * @return List of objects of type mapToClass
+     */
     <T> List<T> mapTo(Collection<?> objects, Class<T> mapToClass);
 
+    /**
+     * Maps given object to an object of given type
+     * @param u object to be mapped
+     * @param mapToClass class to be mapped to
+     * @param <T>
+     * @return Object of type mapToClass
+     */
     <T> T mapTo(Object u, Class<T> mapToClass);
 }
