1488019121130c6452296ddaa8e53d70dedc3878
katuska <katerina.caletkova@gmail.com>
2016-12-08 11:42:20 +0100

    add basic jsp + controller for user

--- web/src/main/java/cz/muni/fi/pa165/travelagency/web/controller/UserController.java
+++ web/src/main/java/cz/muni/fi/pa165/travelagency/web/controller/UserController.java
@@ -4,16 +4,23 @@ import cz.muni.fi.pa165.travelagency.facade.ExcursionFacade;
 import cz.muni.fi.pa165.travelagency.facade.ReservationFacade;
 import cz.muni.fi.pa165.travelagency.facade.TripFacade;
 import cz.muni.fi.pa165.travelagency.facade.UserFacade;
+import cz.muni.fi.pa165.travelagency.facade.dto.ReservationDto;
+import cz.muni.fi.pa165.travelagency.facade.dto.UserDto;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
  * @author Katerina Caletkova
  */
 @Controller
-@RequestMapping(value = {"/admin/users"})
+@RequestMapping(value = {"/admin_user"})
 public class UserController {
     
     @Inject
@@ -32,10 +39,33 @@ public class UserController {
     //userFacade.update(ud); NE
     //userFacade.userAuthenticate(uad); NE
     
-    //userFacade.findAll(); ANO    
     //userFacade.findAll(); ANO
-    //userFacade.findByBirthDate(date); ANO
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String listUsers(Model model){        
+        model.addAttribute("users", userFacade.findAll());
+        return "/admin_user/list";
+    }
+ 
+     //userFacade.findByBirthDate(date); ANO
+    
     //userFacade.findById(Long.MIN_VALUE); ANO
+    @RequestMapping(value = "/index/{id}", method = RequestMethod.GET)
+    public String userDetail(@PathVariable("id") long id, Model model){
+        
+       UserDto user = userFacade.findById(id);
+        if(user == null){
+            return "/admin_user/list";
+        }
+        
+        List<ReservationDto> reservations = reservationFacade.findByUser(user);
+        if(reservations == null){
+            reservations = new ArrayList<>();
+        }
+        
+        model.addAttribute("user", user);
+        model.addAttribute("reservations", reservations);
+        return "/admin_user/index";
+    }
     //userFacade.findByMail(string); ANO
     //userFacade.findByName(string); ANO
     //userFacade.findByPersonalNumber(Long.MIN_VALUE); ANO 
---
+++ web/src/main/webapp/WEB-INF/jsp/admin_user/list.jsp
@@ -0,0 +1,34 @@
+<%@ page contentType="text/html;charset=UTF-8" pageEncoding="utf-8" trimDirectiveWhitespaces="true" session="false" %>
+<%@ taglib tagdir="/WEB-INF/tags" prefix="my" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="s" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+
+<my:pagetemplate title="Users">
+<jsp:attribute name="body">
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>mail</th>
+        </tr>
+        </thead>
+        <tbody>
+        <c:forEach items="${users}" var="user">
+            <tr>
+                <td>${user.id}</td>
+                <td><c:out value="${user.name}"/></td>
+                <td><c:out value="${user.mail}"/></td>
+                <td>
+                    <a href="/pa165/admin_user/index/${user.id}" class="btn btn-primary">Detail</a>
+                </td>
+            </tr>
+        </c:forEach>
+        </tbody>
+    </table>
+</jsp:attribute>
+</my:pagetemplate>
